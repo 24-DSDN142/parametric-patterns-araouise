7d57fb965325336ae3a0af021dbd1c51228372f9
Bela Ditchon <ditchonara@gmail.com>
2024-07-25 11:44:10 +1200

    sketch

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,5 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let eyeSize = 20;
 
 
 function setup_wallpaper(pWallpaper) {
@@ -19,5 +18,32 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(30 ,40, rect_width, rect_height);
+  //function draw() {
+    stroke(128, 172, 242);
+    strokeWeight(3)
+    
+    //ears
+    ellipse(80, 60, 35, 60);
+    ellipse(120, 60, 35, 60);
+    
+    //head
+    ellipse(100, 100, 90, 90);
+    
+    //let eyeSize = 10;
+    //eyes
+    ellipse(80, 85, eyeSize, eyeSize);
+    ellipse(120, 85, eyeSize, eyeSize);
+    
+    //whiskers
+    line(35, 100, 80, 105)
+    line(35, 130, 80, 115)
+    
+    line(120, 105, 165, 100)
+    line(120, 115, 165, 130)
+    
+    //nose
+    //triangle(250, 250, 240, 280, 260, 280)
+    
+    //mouth
+    ellipse(100, 120, 20, 20)
 }
