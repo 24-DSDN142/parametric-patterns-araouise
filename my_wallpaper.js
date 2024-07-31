14e2c07cb9c3e136a023f019fe90c2d0d56a5292
Bela Ditchon <ditchonara@gmail.com>
2024-08-01 02:18:36 +1200

    updated sketch

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,9 +1,8 @@
 //your parameter variables go here!
-let eyeSize = 30;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
@@ -19,31 +18,76 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   //function draw() {
-    stroke(128, 172, 242); //blue
-    strokeWeight(3)
+    stroke(0);
+    strokeWeight(1);
+    angleMode(DEGREES);
     
-    //ears
-    ellipse(80, 60, 35, 60);
-    ellipse(120, 60, 35, 60);
+    beginShape(); //left ear
+      vertex(60, 45);
+      vertex(53, 10);
+      vertex(85, 13);
+    endShape(CLOSE);
+    
+    beginShape(); //right ear
+      vertex(140, 45);
+      vertex(116, 14);
+      vertex(150, 11);
+    endShape(CLOSE);
+    
+    //tail
+    strokeCap(ROUND);
+    beginShape();
+    vertex(140, 170);
+    bezierVertex(165, 3, 180, 100, 150, 170);
+    endShape(CLOSE);
+    
+    //body
+    beginShape();
+    vertex(40, 170);
+    //quadraticVertex(50, 1, 165, 35);
+    bezierVertex(75, 5, 140, 60, 160, 170);
+    endShape(CLOSE);
     
     //head
-    ellipse(100, 100, 90, 90);
-    
-    //let eyeSize = 10;
-    //eyes
-    ellipse(80, 85, eyeSize, eyeSize);
-    ellipse(120, 85, eyeSize, eyeSize);
-    
-    //whiskers
-    line(35, 100, 80, 105)
-    line(35, 130, 80, 115)
-    
-    line(120, 105, 165, 100)
-    line(120, 115, 165, 130)
+    ellipse(100, 50, 80, 80); 
     
     //nose
-    //triangle(250, 250, 240, 280, 260, 280)
+    triangle(95, 50, 100, 55, 105, 50);
+    
+    //eye
+    ellipse(80, 35, 20, 15);
+    ellipse(120, 35, 20, 15);
+    
+    //eyeballs
+    ellipse(80, 35, 12, 15);
+    ellipse(120, 35, 12, 15);
+    
+    //middle of the eye
+    //line(80, 34, 80, 36);
+    //line(120, 34, 120, 36);
     
     //mouth
-    ellipse(100, 120, 20, 20)
+    arc(95, 50, 15, 25, 60, 120);
+    arc(105, 50, 15, 25, 60, 120);
+    line(100, 55, 100, 60);
+    
+    //legs
+    line(70, 140, 70, 170);
+    line(95, 140, 95, 170);
+    line(105, 140, 105, 170);
+    line(130, 140, 130, 170);
+    
+    //toes
+    let toeSize = 11
+    let toeY = 170
+    
+    arc(73, toeY, toeSize, toeSize, 180, 0); //far left toebean
+    arc(83, toeY, toeSize, toeSize, 180, 0);
+    arc(93, toeY, toeSize, toeSize, 180, 0);
+    
+    arc(107, toeY, toeSize, toeSize, 180, 0);
+    arc(117, toeY, toeSize, toeSize, 180, 0);
+    arc(127, toeY, toeSize, toeSize, 180, 0); //far right toebean
+    
 }
+
